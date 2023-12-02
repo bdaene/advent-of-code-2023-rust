1d8d09c9eb43e74780ee601cd15e829c038e7fef
Benoît Daene <benoitdaene@gmail.com>
2023-12-02 13:56:04 +0100

    Improve day 02

--- src/days/day_02.rs
+++ src/days/day_02.rs
@@ -1,130 +1,129 @@
-use std::ops;
-
-use nom::{branch, bytes::complete::tag, character::complete, IResult, multi, sequence};
+use nom::branch::alt;
+use nom::character::complete;
+use nom::multi::separated_list1;
+use nom::sequence::separated_pair;
+use nom::{bytes::complete::tag, IResult, Parser};
+use nom_supreme::ParserExt;
 
 use crate::PuzzleBase;
 
-#[derive(PartialEq, Debug, Default, Clone, Copy)]
-pub struct Color {
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+enum Color {
+    Red,
+    Green,
+    Blue,
+}
+
+#[derive(Debug, PartialEq, Eq, Default)]
+struct CubeSubset {
     red: u32,
     green: u32,
     blue: u32,
 }
 
-#[derive(PartialEq, Debug)]
+#[derive(Debug, PartialEq, Eq)]
 pub struct Game {
     id: u32,
-    colors: Vec<Color>,
+    cube_subsets: Vec<CubeSubset>,
 }
 
-
-#[derive(PartialEq, Debug)]
+#[derive(Debug, PartialEq, Eq)]
 pub struct Puzzle {
     games: Vec<Game>,
 }
 
-
-impl ops::Add for Color {
-    type Output = Color;
-
-    fn add(self, color: Self) -> Self::Output {
-        Color {
-            red: self.red + color.red,
-            green: self.green + color.green,
-            blue: self.blue + color.blue,
-        }
+impl Color {
+    fn parse(input: &str) -> IResult<&str, Self> {
+        alt((
+            tag("red").value(Color::Red),
+            tag("green").value(Color::Green),
+            tag("blue").value(Color::Blue),
+        ))
+            .parse(input)
     }
 }
 
-impl Color {
-    fn from_color_str(input: &str) -> IResult<&str, Color> {
-        let (input, (n, color)) = branch::alt((
-            sequence::separated_pair(complete::u32, complete::multispace1, tag("red")),
-            sequence::separated_pair(complete::u32, complete::multispace1, tag("green")),
-            sequence::separated_pair(complete::u32, complete::multispace1, tag("blue")),
-        ))(input)?;
-        Ok((input,
-            match color {
-                "red" => Color { red: n, ..Color::default() },
-                "green" => Color { green: n, ..Color::default() },
-                "blue" => Color { blue: n, ..Color::default() },
-                _ => panic!("Invalid color {:?}.", color)
-            }
-        ))
-    }
-
-    fn from_str(input: &str) -> IResult<&str, Color> {
-        let (input, colors) = multi::separated_list1(
+impl CubeSubset {
+    fn parse(input: &str) -> IResult<&str, Self> {
+        separated_list1(
             tag(", "),
-            Color::from_color_str,
-        )(input)?;
-        Ok((input,
-            colors.into_iter().fold(Color::default(), |a, b| a + b)
-        ))
+            separated_pair(complete::u32, complete::space1, Color::parse),
+        )
+            .map(|cube_subsets| {
+                cube_subsets
+                    .iter()
+                    .fold(CubeSubset::default(), |acc, (n, color)| match color {
+                        Color::Red => CubeSubset { red: acc.red + n, ..acc },
+                        Color::Green => CubeSubset { green: acc.green + n, ..acc },
+                        Color::Blue => CubeSubset { blue: acc.blue + n, ..acc },
+                    })
+            })
+            .parse(input)
     }
 
-    fn max(colors: &[Color]) -> Option<Color> {
-        let mut iterator = colors.iter();
-        let max_color = iterator.next()?;
-        Some(iterator.fold(
-            *max_color,
-            |a, b| Color {
-                red: a.red.max(b.red),
-                green: a.green.max(b.green),
-                blue: a.blue.max(b.blue),
-            },
-        ))
-    }
-
-    fn power(&self) -> u32 {
+    fn get_power(&self) -> u32 {
         self.red * self.green * self.blue
     }
 }
 
 impl Game {
-    fn from_str(input: &str) -> IResult<&str, Game> {
-        let (input, (_, id, _, colors)) = sequence::tuple((
-            tag("Game "),
-            complete::u32,
+    fn parse(input: &str) -> IResult<&str, Self> {
+        separated_pair(
+            tag("Game ").precedes(complete::u32),
             tag(": "),
-            multi::separated_list1(
-                tag("; "),
-                Color::from_str,
-            )
-        ))(input)?;
-        Ok((input,
-            Game { id, colors }
-        ))
+            separated_list1(tag("; "), CubeSubset::parse),
+        )
+            .map(|(id, cube_subsets)| Self { id, cube_subsets })
+            .parse(input)
+    }
+
+    fn is_possible(&self) -> bool {
+        self.cube_subsets
+            .iter()
+            .all(|cube_subset| {
+            cube_subset.red <= 12 && cube_subset.green <= 13 && cube_subset.blue <= 14
+        })
+    }
+
+    fn get_min_cube_subset(&self) -> CubeSubset {
+        self.cube_subsets
+            .iter()
+            .fold(CubeSubset::default(), |acc, cube_subset| CubeSubset {
+                red: acc.red.max(cube_subset.red),
+                green: acc.green.max(cube_subset.green),
+                blue: acc.blue.max(cube_subset.blue),
+            })
+    }
+}
+
+impl Puzzle {
+    fn parse(input: &str) -> IResult<&str, Self> {
+        separated_list1(complete::line_ending, Game::parse)
+            .map(|games| Self { games })
+            .parse(input)
     }
 }
 
 impl PuzzleBase for Puzzle {
     fn new(data: &str) -> Self {
-        let games = data.lines()
-            .map(|line| {
-                let (_, game) = Game::from_str(line).unwrap();
-                game
-            })
-            .collect();
-        Puzzle { games }
+        Self::parse(data).unwrap().1
     }
 
     fn part_1(&self) -> String {
-        self.games.iter()
-            .filter(|&game| {
-                game.colors.iter()
-                    .all(|colors| colors.red <= 12 && colors.green <= 13 && colors.blue <= 14)
-            })
+        self.games
+            .iter()
+            .filter(|game| game.is_possible())
             .map(|game| game.id)
             .sum::<u32>()
             .to_string()
     }
 
     fn part_2(&self) -> String {
-        self.games.iter()
-            .map(|game| Color::max(&game.colors[..]).unwrap())
-            .map(|color| color.power())
-            .sum::<u32>().to_string()
+        self.games
+            .iter()
+            .map(|game| game.get_min_cube_subset().get_power())
+            .sum::<u32>()
+            .to_string()
     }
 }
 
@@ -150,41 +149,41 @@ mod test {
             games: vec![
                 Game {
                     id: 1,
-                    colors: vec![
-                        Color { red: 4, blue: 3, green: 0 },
-                        Color { red: 1, blue: 6, green: 2 },
-                        Color { red: 0, blue: 0, green: 2 },
+                    cube_subsets: vec![
+                        CubeSubset { red: 4, blue: 3, green: 0 },
+                        CubeSubset { red: 1, blue: 6, green: 2 },
+                        CubeSubset { red: 0, blue: 0, green: 2 },
                     ],
                 },
                 Game {
                     id: 2,
-                    colors: vec![
-                        Color { red: 0, blue: 1, green: 2 },
-                        Color { red: 1, blue: 4, green: 3 },
-                        Color { red: 0, blue: 1, green: 1 },
+                    cube_subsets: vec![
+                        CubeSubset { red: 0, blue: 1, green: 2 },
+                        CubeSubset { red: 1, blue: 4, green: 3 },
+                        CubeSubset { red: 0, blue: 1, green: 1 },
                     ],
                 },
                 Game {
                     id: 3,
-                    colors: vec![
-                        Color { red: 20, blue: 6, green: 8 },
-                        Color { red: 4, blue: 5, green: 13 },
-                        Color { red: 1, blue: 0, green: 5 },
+                    cube_subsets: vec![
+                        CubeSubset { red: 20, blue: 6, green: 8 },
+                        CubeSubset { red: 4, blue: 5, green: 13 },
+                        CubeSubset { red: 1, blue: 0, green: 5 },
                     ],
                 },
                 Game {
                     id: 4,
-                    colors: vec![
-                        Color { red: 3, blue: 6, green: 1 },
-                        Color { red: 6, blue: 0, green: 3 },
-                        Color { red: 14, blue: 15, green: 3 },
+                    cube_subsets: vec![
+                        CubeSubset { red: 3, blue: 6, green: 1 },
+                        CubeSubset { red: 6, blue: 0, green: 3 },
+                        CubeSubset { red: 14, blue: 15, green: 3 },
                     ],
                 },
                 Game {
                     id: 5,
-                    colors: vec![
-                        Color { red: 6, blue: 1, green: 3 },
-                        Color { red: 1, blue: 2, green: 2 },
+                    cube_subsets: vec![
+                        CubeSubset { red: 6, blue: 1, green: 3 },
+                        CubeSubset { red: 1, blue: 2, green: 2 },
                     ],
                 },
             ]
@@ -203,5 +202,11 @@ mod test {
         let solution = get_solution();
 
         assert_eq!(solution.part_2(), "2286");
+    }
+
+    #[test]
+    fn cube_subset_parse() {
+        assert_eq!(CubeSubset::parse("1 red, 2 green, 3 blue"), Ok(("", CubeSubset { red: 1, green: 2, blue: 3 })));
+        assert_eq!(CubeSubset::parse("4 blue"), Ok(("", CubeSubset { blue: 4, ..CubeSubset::default() })));
     }
 }
