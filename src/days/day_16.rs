93de36298827203182da0038ca1499452728d603
Benoît Daene <benoitdaene@gmail.com>
2023-12-16 19:38:28 +0100

    Improve day 16.

--- src/days/day_16.rs
+++ src/days/day_16.rs
@@ -1,16 +1,43 @@
 use nom::{IResult, Parser};
-use nom::bytes::complete::take_till1;
+use nom::branch::alt;
 use nom::character::complete;
-use nom::multi::separated_list1;
+use nom::multi::{many1, separated_list1};
+use nom_supreme::ParserExt;
+use nom_supreme::tag::complete::tag;
 
 use crate::PuzzleBase;
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct Puzzle {
-    grid: Vec<Vec<char>>,
+    grid: Vec<Vec<Option<Object>>>,
 }
 
 #[derive(Debug, PartialEq, Eq, Copy, Clone)]
+enum Object {
+    Mirror(Mirror),
+    Splitter(Splitter),
+}
+
+#[derive(Debug, PartialEq, Eq, Copy, Clone)]
+enum Mirror {
+    Ascending,
+    Descending,
+}
+
+#[derive(Debug, PartialEq, Eq, Copy, Clone)]
+enum Splitter {
+    Vertical,
+    Horizontal,
+}
+
+#[derive(Debug, PartialEq, Eq, Copy, Clone, Hash)]
+struct LightBeam {
+    row: usize,
+    col: usize,
+    direction: Direction,
+}
+
+#[derive(Debug, PartialEq, Eq, Copy, Clone, Hash)]
 enum Direction {
     Up,
     Down,
@@ -18,123 +45,111 @@ enum Direction {
     Right,
 }
 
-#[derive(Debug, PartialEq, Eq, Copy, Clone)]
-struct Position {
-    row: usize,
-    col: usize,
-}
 
 impl PuzzleBase for Puzzle {
     fn parse(input: &str) -> IResult<&str, Self> {
         separated_list1(
             complete::line_ending,
-            take_till1(|c| "\r\n".contains(c)),
+            many1(alt((
+                tag(".").value(None),
+                tag("/").value(Some(Object::Mirror(Mirror::Ascending))),
+                tag("\\").value(Some(Object::Mirror(Mirror::Descending))),
+                tag("|").value(Some(Object::Splitter(Splitter::Vertical))),
+                tag("-").value(Some(Object::Splitter(Splitter::Horizontal))),
+            ))),
         )
-            .map(|lines| Self {
-                grid: lines.into_iter()
-                    .map(|line: &str| line.chars().collect())
-                    .collect()
-            })
+            .map(|grid| Self { grid })
             .parse(input)
     }
 
     fn part_1(&self) -> String {
-        energize(&self.grid, Position { row: 0, col: 0 }, Direction::Right)
+        energize(&self.grid, LightBeam { row: 0, col: 0, direction: Direction::Right })
             .to_string()
     }
 
     fn part_2(&self) -> String {
         let (height, width) = (self.grid.len(), self.grid[0].len());
 
-        let max_right = (0..height)
-            .map(|row| energize(&self.grid, Position{row, col:0}, Direction::Right))
-            .max().unwrap();
-        let max_down = (0..width)
-            .map(|col| energize(&self.grid, Position{row:0, col}, Direction::Down))
-            .max().unwrap();
-        let max_left = (0..height)
-            .map(|row| energize(&self.grid, Position{row, col:width-1}, Direction::Left))
-            .max().unwrap();
-        let max_up = (0..width)
-            .map(|col| energize(&self.grid, Position{row:height-1, col}, Direction::Up))
-            .max().unwrap();
-
-        max_right.max(max_down).max(max_left).max(max_up).to_string()
+        0
+            .max(
+                (0..height)
+                    .map(|row| energize(&self.grid, LightBeam { row, col: 0, direction: Direction::Right }))
+                    .max().unwrap()
+            )
+            .max(
+                (0..width)
+                    .map(|col| energize(&self.grid, LightBeam { row: 0, col, direction: Direction::Down }))
+                    .max().unwrap()
+            )
+            .max(
+                (0..height)
+                    .map(|row| energize(&self.grid, LightBeam { row, col: width - 1, direction: Direction::Left }))
+                    .max().unwrap()
+            )
+            .max(
+                (0..width)
+                    .map(|col| energize(&self.grid, LightBeam { row: height - 1, col, direction: Direction::Up }))
+                    .max().unwrap()
+            )
+            .to_string()
     }
 }
 
 impl Direction {
-    fn bounce(&self, cell: char) -> Vec<Direction> {
-        match cell {
-            '.' => vec![*self],
-            '/' => match self {
-                Direction::Up => vec![Direction::Right],
-                Direction::Down => vec![Direction::Left],
-                Direction::Left => vec![Direction::Down],
-                Direction::Right => vec![Direction::Up],
+    fn bounce(&self, object: Object) -> [Option<Direction>; 2] {
+        match object {
+            Object::Mirror(mirror) => match (mirror, self) {
+                (Mirror::Ascending, Direction::Right) | (Mirror::Descending, Direction::Left) => [Some(Direction::Up), None],
+                (Mirror::Ascending, Direction::Left) | (Mirror::Descending, Direction::Right) => [Some(Direction::Down), None],
+                (Mirror::Ascending, Direction::Up) | (Mirror::Descending, Direction::Down) => [Some(Direction::Right), None],
+                (Mirror::Ascending, Direction::Down) | (Mirror::Descending, Direction::Up) => [Some(Direction::Left), None],
             },
-            '\\' => match self {
-                Direction::Up => vec![Direction::Left],
-                Direction::Down => vec![Direction::Right],
-                Direction::Left => vec![Direction::Up],
-                Direction::Right => vec![Direction::Down],
-            },
-            '-' => match self {
-                Direction::Up => vec![Direction::Left, Direction::Right],
-                Direction::Down => vec![Direction::Right, Direction::Left],
-                Direction::Left => vec![Direction::Left],
-                Direction::Right => vec![Direction::Right],
-            },
-            '|' => match self {
-                Direction::Up => vec![Direction::Up],
-                Direction::Down => vec![Direction::Down],
-                Direction::Left => vec![Direction::Up, Direction::Down],
-                Direction::Right => vec![Direction::Down, Direction::Up],
-            },
-            _ => panic!("Unknown cell {cell}")
+            Object::Splitter(splitter) => match (splitter, self) {
+                (Splitter::Horizontal, Direction::Up) | (Splitter::Horizontal, Direction::Down) => [Some(Direction::Left), Some(Direction::Right)],
+                (Splitter::Vertical, Direction::Left) | (Splitter::Vertical, Direction::Right) => [Some(Direction::Up), Some(Direction::Down)],
+                _ => [Some(*self), None]
+            }
         }
     }
 }
 
-impl Position {
-    fn update(&self, direction: &Direction, height: usize, width: usize) -> Option<Position> {
-        let (row, col) = (self.row, self.col);
-        match direction {
-            Direction::Up => if self.row > 0 { Some(Position { row: row - 1, col }) } else { None },
-            Direction::Left => if self.col > 0 { Some(Position { row, col: col - 1 }) } else { None },
-            Direction::Right => if self.col + 1 < width { Some(Position { row, col: col + 1 }) } else { None },
-            Direction::Down => if self.row + 1 < height { Some(Position { row: row + 1, col }) } else { None },
-        }
+impl LightBeam {
+    fn update(&self, direction: Direction, limits: (usize, usize)) -> Option<Self> {
+        let (height, width) = limits;
+        Some(match direction {
+            Direction::Up => LightBeam { row: self.row.checked_sub(1)?, col: self.col, direction },
+            Direction::Left => LightBeam { row: self.row, col: self.col.checked_sub(1)?, direction },
+            Direction::Down => (self.row + 1 < height).then_some(LightBeam { row: self.row + 1, col: self.col, direction })?,
+            Direction::Right => (self.col + 1 < width).then_some(LightBeam { row: self.row, col: self.col + 1, direction })?,
+        })
+    }
+
+    fn bounce(&self, object: Object, limits: (usize, usize)) -> [Option<LightBeam>; 2] {
+        self.direction.bounce(object)
+            .map(|direction| direction.and_then(|direction| self.update(direction, limits)))
     }
 }
 
-fn energize(grid: &Vec<Vec<char>>, position: Position, direction: Direction) -> usize {
-    let (height, width) = (grid.len(), grid[0].len());
-    let mut states = vec![(position, direction)];
 
-    let mut seen = vec![vec![vec![false; width]; height]; 4];
+fn energize(grid: &Vec<Vec<Option<Object>>>, light_beam: LightBeam) -> usize {
+    let limits = (grid.len(), grid[0].len());
+    let (height, width) = limits;
+    let mut light_beams = vec![light_beam];
 
-    while let Some((position, direction)) = states.pop() {
-        if seen[direction as usize][position.row][position.col] {
-            continue;
-        }
-        seen[direction as usize][position.row][position.col] = true;
-        for direction_ in direction.bounce(grid[position.row][position.col]) {
-            if let Some(position_) = position.update(&direction_, height, width) {
-                states.push((position_, direction_))
+    let mut energized = vec![vec![0u8; width]; height];
+
+    while let Some(light_beam) = light_beams.pop() {
+        let flag = 1u8 << light_beam.direction as u8;
+        if energized[light_beam.row][light_beam.col] & flag == 0 {
+            energized[light_beam.row][light_beam.col] |= flag;
+            match grid[light_beam.row][light_beam.col] {
+                None => light_beam.update(light_beam.direction, limits).into_iter().for_each(|light_beam| light_beams.push(light_beam)),
+                Some(object) => light_beams.extend(light_beam.bounce(object, limits).into_iter().flatten())
             }
         }
     }
 
-    (0..height)
-        .flat_map(|row| (0..width)
-            .map(move |col| (row, col))
-            .filter(|&(row, col)| seen[Direction::Up as usize][row][col]
-                | seen[Direction::Down as usize][row][col]
-                | seen[Direction::Left as usize][row][col]
-                | seen[Direction::Right as usize][row][col]
-            ))
-        .count()
+    energized.into_iter().flat_map(|line| line.into_iter().filter(|&cell| cell != 0)).count()
 }
 
 
@@ -158,16 +173,16 @@ mod test {
 
         assert_eq!(puzzle, Puzzle {
             grid: vec![
-                vec!['.', '|', '.', '.', '.', '\\', '.', '.', '.', '.'],
-                vec!['|', '.', '-', '.', '\\', '.', '.', '.', '.', '.'],
-                vec!['.', '.', '.', '.', '.', '|', '-', '.', '.', '.'],
-                vec!['.', '.', '.', '.', '.', '.', '.', '.', '|', '.'],
-                vec!['.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
-                vec!['.', '.', '.', '.', '.', '.', '.', '.', '.', '\\'],
-                vec!['.', '.', '.', '.', '/', '.', '\\', '\\', '.', '.'],
-                vec!['.', '-', '.', '-', '/', '.', '.', '|', '.', '.'],
-                vec!['.', '|', '.', '.', '.', '.', '-', '|', '.', '\\'],
-                vec!['.', '.', '/', '/', '.', '|', '.', '.', '.', '.'],
+                vec![None, Some(Object::Splitter(Splitter::Vertical)), None, None, None, Some(Object::Mirror(Mirror::Descending)), None, None, None, None],
+                vec![Some(Object::Splitter(Splitter::Vertical)), None, Some(Object::Splitter(Splitter::Horizontal)), None, Some(Object::Mirror(Mirror::Descending)), None, None, None, None, None],
+                vec![None, None, None, None, None, Some(Object::Splitter(Splitter::Vertical)), Some(Object::Splitter(Splitter::Horizontal)), None, None, None],
+                vec![None, None, None, None, None, None, None, None, Some(Object::Splitter(Splitter::Vertical)), None],
+                vec![None, None, None, None, None, None, None, None, None, None],
+                vec![None, None, None, None, None, None, None, None, None, Some(Object::Mirror(Mirror::Descending))],
+                vec![None, None, None, None, Some(Object::Mirror(Mirror::Ascending)), None, Some(Object::Mirror(Mirror::Descending)), Some(Object::Mirror(Mirror::Descending)), None, None],
+                vec![None, Some(Object::Splitter(Splitter::Horizontal)), None, Some(Object::Splitter(Splitter::Horizontal)), Some(Object::Mirror(Mirror::Ascending)), None, None, Some(Object::Splitter(Splitter::Vertical)), None, None],
+                vec![None, Some(Object::Splitter(Splitter::Vertical)), None, None, None, None, Some(Object::Splitter(Splitter::Horizontal)), Some(Object::Splitter(Splitter::Vertical)), None, Some(Object::Mirror(Mirror::Descending))],
+                vec![None, None, Some(Object::Mirror(Mirror::Ascending)), Some(Object::Mirror(Mirror::Ascending)), None, Some(Object::Splitter(Splitter::Vertical)), None, None, None, None],
             ]
         })
     }
